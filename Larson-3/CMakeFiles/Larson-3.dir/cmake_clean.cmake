eb9170d69d894ef6a98005f16f5120d93b1cc856
Devansh Sonigra <devansh.sonigra@gmail.com>
2025-05-31 11:50:33 +0530

    Initial commit

---
+++ Larson-3/CMakeFiles/Larson-3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Larson-3.dir/Larson-3.cc.o"
+  "CMakeFiles/Larson-3.dir/Larson-3.cc.o.d"
+  "Larson-3"
+  "Larson-3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Larson-3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Larson-3/CMakeFiles/Projection.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projection.dir/Projection.cc.o"
+  "CMakeFiles/Projection.dir/Projection.cc.o.d"
+  "Projection"
+  "Projection.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Projection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
